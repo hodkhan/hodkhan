f25d11b3702674f9ed4f4ac5354df846e9ac6a29
mahdiahmadi87 <mahdi.ahmadi.1387@gmail.com>
2025-01-11 19:15:42 +0330

    changed fasttext model
    Former-commit-id: 26a5a66eb4d272145b29e4b920b6cf550f11782d

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -1,4 +1,4 @@
-import fasttext.util
+import compress_fasttext
 import feedparser
 import requests 
 import signal
@@ -21,8 +21,12 @@ def timeout_handler(signum, frame):
     raise TimeoutException()
 
 # Load FastText model
-model_path = './../cc.fa.300.bin'  # مسیر مدل دانلودشده
-fasttext_model = fasttext.load_model(model_path)
+# model_path = './../cc.fa.300.bin'  # مسیر مدل دانلودشده
+# fasttext_model = fasttext.load_model(model_path)
+model_path = './../cc.fa.300.q.bin'  # مسیر مدل دانلودشده
+fasttext_model = compress_fasttext.models.CompressedFastTextKeyedVectors.load(model_path)
+
+
 
 def vectorize_text(text):
     """Convert text to vector using FastText."""
--- crawlers/updateLastNews.py
+++ crawlers/updateLastNews.py
@@ -1,10 +1,14 @@
-import sqlite3
-import fasttext
+import compress_fasttext
 import numpy as np
+import sqlite3
 
 # Load FastText model
-model_path = './../cc.fa.300.bin'  # مسیر مدل دانلودشده
-fasttext_model = fasttext.load_model(model_path)
+# model_path = './../cc.fa.300.bin'  # مسیر مدل دانلودشده
+# fasttext_model = fasttext.load_model(model_path)
+model_path = './../cc.fa.300.q.bin'  # مسیر مدل دانلودشده
+fasttext_model = compress_fasttext.models.CompressedFastTextKeyedVectors.load(model_path)
+
+
 
 def vectorize_text(text):
     """Convert text to vector using FastText."""
